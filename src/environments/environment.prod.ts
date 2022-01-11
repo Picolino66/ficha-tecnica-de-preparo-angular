f3bb41d8b0cee249fc33bbf31550299292defe39
Picolino66 <isaiasbd@hotmail.com>
2022-01-11 01:05:13 -0300

    subindo evironment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBsmGTxjEpIl21xMuG2G5dJqG0b9Zu-O-U",
+    authDomain: "ficha-tecnica-de-preparo.firebaseapp.com",
+    projectId: "ficha-tecnica-de-preparo",
+    storageBucket: "ficha-tecnica-de-preparo.appspot.com",
+    messagingSenderId: "653300606093",
+    appId: "1:653300606093:web:4bf15b8c51258bb0eb2ec4",
+    measurementId: "G-B7XE3L7GEK"
+  },
 };
